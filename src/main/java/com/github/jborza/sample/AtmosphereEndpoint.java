49f2512ba6f196d278ea5c41171d736d3f7fca41
jborza <jborza@gmail.com>
2019-02-03 11:25:48 +0100

    project creation + initial Atmosphere endpoint

---
+++ src/main/java/com/github/jborza/sample/AtmosphereEndpoint.java
@@ -0,0 +1,52 @@
+package com.github.jborza.sample;
+
+import org.atmosphere.config.service.Disconnect;
+import org.atmosphere.config.service.Heartbeat;
+import org.atmosphere.config.service.ManagedService;
+import org.atmosphere.config.service.Ready;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.Broadcaster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import static org.atmosphere.cpr.ApplicationConfig.MAX_INACTIVE;
+
+@ManagedService(
+        path = AtmosphereEndpoint.ENDPOINT_URL,
+        atmosphereConfig = MAX_INACTIVE + "=10000"
+)
+public class AtmosphereEndpoint {
+    private final Logger logger = LoggerFactory.getLogger(AtmosphereEndpoint.class);
+    public final static String ENDPOINT_URL = "/atmo";
+
+    @Inject
+    @Named(ENDPOINT_URL)
+    private Broadcaster broadcaster;
+
+    @Ready
+    public void onReady(final AtmosphereResource r) {
+        logger.info("Browser {} connected", r.uuid());
+    }
+
+    @Disconnect
+    public void onDisconnect(final AtmosphereResourceEvent event) {
+        logger.info("Browser {} disconnected", event.getResource().uuid());
+        logger.info("Event message = " + event.getMessage());
+    }
+
+    @Heartbeat
+    public void onHeartbeat(final AtmosphereResourceEvent event) {
+        logger.info("Heartbeat sent by {}", event.getResource().uuid());
+    }
+
+    public void broadcast(String message) {
+        if (broadcaster == null)
+            return;
+        logger.info("Broadcasting {}", message);
+        broadcaster.broadcast(message);
+    }
+}
